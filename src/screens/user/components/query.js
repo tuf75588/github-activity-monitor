4bc3c85c467d74a8e81793d1022b109059ddac3d
Andrew Davis <atd285@gmail.com>
2019-09-19 20:04:26 -0400

    begin work on Query component

--- src/screens/user/components/query.js
+++ src/screens/user/components/query.js
@@ -1,16 +1,7 @@
 import React, {useReducer, useEffect, useContext, useRef} from 'react';
 import PropTypes from 'prop-types';
 import * as Github from '../../../github-client';
-
-function useSafeSetState(initialState) {
-  return useReducer(
-    (state, newState) => ({
-      ...state,
-      ...newState,
-    }),
-    initialState,
-  );
-}
+import isEqual from 'lodash/isEqual';
 
 function usePrevious(value) {
   const ref = useRef();
@@ -20,21 +11,48 @@ function usePrevious(value) {
   return ref.current;
 }
 
-function useDeepCompare() {
-  const mountedRef = useRef(false);
+function useSetState(initialState) {
+  return useReducer(
+    (state, newState) => ({
+      ...state,
+      ...newState,
+    }),
+    initialState,
+  );
 }
 
-function Query({normalize = (data) => data, children, query, variables}) {
-  const [state, setState] = useSafeSetState({
+function useSafeSetState(initialState) {
+  const [state, setState] = useSetState(initialState);
+  const mountedRef = useRef(false);
+  useEffect(() => {
+    mountedRef.current = true;
+    return () => {
+      mountedRef.current = false;
+    };
+  }, []);
+  const safeSetState = (...args) => mountedRef.current && setState(...args);
+  return [state, safeSetState];
+}
+
+function useQuery({normalize = (data) => data, children, query, variables}) {
+  const [state, safeSetState] = useSafeSetState({
+    loaded: false,
+    fetching: false,
     data: null,
-    loading: null,
-    normalize: null,
     error: null,
   });
-  const client = useContext(Github.Context);
-  useEffect(() => {}, [query, variables]);
-  return children(state);
+
+  useEffect(() => {
+    if (isEqual(previousInputs, [query, variables])) {
+      return;
+    }
+    setState({fetching: true});
+  });
+
+  const previousInputs = usePrevious([query, variables]);
 }
+
+const Query = ({children, props}) => children(useQuery(props));
 
 Query.propTypes = {
   normalize: PropTypes.func,
--- src/screens/user/index.js
+++ src/screens/user/index.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, {useState, useContext} from 'react';
+import {Context as GithubContext} from '../../github-client';
 
-function User() {
+const gql = String.raw;
+
+//! user will receive a username prop for our graphql query
+function User({username}) {
+  const [filter, setfilter] = useState('');
+  const {logout} = useContext(GithubContext);
+
   return <div>USER!</div>;
 }
 
