7ffa45d8e70d590d8e7e9fb5db03323310705159
Andrew Davis <atd285@gmail.com>
2019-09-25 21:56:35 -0400

    move util style functions to own file

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -6,88 +6,13 @@ import {useQuery} from '../user/components/query';
 import styled from '@emotion/styled/macro';
 import {jsx} from '@emotion/core';
 
-const smallerHeading = {
-  marginTop: '10px',
-  marginBottom: '10px',
-};
-
-const heading = {
-  display: 'block',
-  fontFamily: 'inherit',
-  fontWeight: 500,
-  lineHeight: 1.1,
-};
-
-export const Text = styled.span(
-  variantStyles({
-    tint: {
-      faded: ({theme}) => ({color: theme.colors.faded}),
-      fadedExtra: ({theme}) => ({color: theme.colors.fadedExtra}),
-      background: ({theme}) => ({backgroundColor: theme.colors.backgroundFallback}),
-    },
-    size: {
-      subheading: [heading, smallerHeading, {fontSize: '25'}],
-    },
-  }),
-);
-
-export const Image = styled.img(
-  {border: 0, verticalAlign: 'middle'},
-  propStyles({
-    responsive: {
-      display: 'block',
-      maxWidth: '100%',
-      height: 'auto',
-    },
-    rounded: {
-      borderRadius: '6px',
-    },
-  }),
-);
-
-//! this works because when we render our component we are invoking our closure
-function variantStyles(styles) {
-  //! accepts a styles object
-  //! returns a new function
-  return function dynamicStyles(props) {
-    return Object.entries(props).map(([key, value]) => {
-      if (styles[key]) {
-        return applyStyles(styles[key][value], props);
-      }
-      return null;
-    });
-  };
-}
-
-function applyStyles(styles, props) {
-  return typeof styles === 'function'
-    ? styles(props)
-    : Array.isArray(styles)
-    ? styles.map((style) => applyStyles(style, props))
-    : styles;
-}
-
-function propStyles(styles) {
-  return function dynamicStyles(props) {
-    return Object.keys(props).map((key) => {
-      if (styles[key]) {
-        return applyStyles(styles[key], props);
-      }
-      return null;
-    });
-  };
-}
-
 //! keys are src, className, theme
 //! styles is an object {responsive: display, height, maxWidth }
 //!
 function Home({username}) {
   return (
     <IsolatedContainer>
-      <Text tint="background" size="subheading">
-        Hello
-        <Image src="https://github.com/tylermcginnis.png" responsive rounded />
-      </Text>
+      <p>home component!</p>
     </IsolatedContainer>
   );
 }
--- src/shared/pattern.js
+++ src/shared/pattern.js
@@ -3,6 +3,58 @@ import React from 'react';
 import {jsx} from '@emotion/core';
 import {Link} from '@reach/router';
 import styled from '@emotion/styled';
+import {style} from 'styled-system';
+//! util functions
+//#region
+//! gets passed a styles object
+function variantStyles(styles) {
+  return function dynamicStyles(props) {
+    return Object.entries(props).map(([key, value]) => {
+      if (styles[key]) {
+        return applyStyles(styles[key][value], props);
+      }
+      return null;
+    });
+  };
+}
+
+function propStyles(styles) {
+  return function dynamicStyles(props) {
+    return Object.keys(props).map((key) => {
+      if (styles[key]) {
+        return applyStyles(styles[key], props);
+      }
+      return null;
+    });
+  };
+}
+
+//! will accept two arguments, check typeof styles and if function call styles with props
+function applyStyles(styles, props) {
+  return typeof styles === 'function'
+    ? styles(props)
+    : Array.isArray(styles)
+    ? style.map((s) => applyStyles(s, props))
+    : styles;
+}
+//#endregion
+
+const heading = {
+  display: 'block',
+  fontFamily: 'inherit',
+  fontWeight: 500,
+  lineHeight: 1.1,
+};
+
+const largeHeading = {
+  marginTop: '20px',
+  marginBottom: '10px',
+};
+
+const smallerHeading = {
+  marginTop: '10px',
+  marginBottom: '10px;',
+};
 
 export const Button = styled.button({
   display: 'inline-block',
@@ -52,36 +104,3 @@ export function IsolatedContainer({children, ...props}) {
     </div>
   );
 }
-
-const heading = {
-  display: 'block',
-  fontFamily: 'inherit',
-  fontWeight: 500,
-  lineHeight: 1.1,
-};
-
-const largeHeading = {
-  marginTop: '20px',
-  marginBottom: '10px',
-};
-
-const smallerHeading = {
-  marginTop: '10px',
-  marginBottom: '10px;',
-};
-
-//! gets passed a styles object
-function variantStyles(styles) {
-  return function dynamicStyles(props) {
-    return Object.entries(props).map((s) => {
-      console.log(s);
-    });
-  };
-}
-
-//! will accept two arguments, check typeof styles and if function call styles with props
-function applyStyles(styles) {
-  console.log(typeof styles);
-}
-
-variantStyles({tint: {faded: ({theme}) => ({color: 'red'})}});
