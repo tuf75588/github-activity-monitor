6cf5079109c8efa9b53dfa8e13c9c0cef881f56a
Andrew Davis <atd285@gmail.com>
2019-09-25 20:07:20 -0400

    add function for applying prop styles

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,7 +1,22 @@
+/* @jsx jsx */
+
 import React from 'react';
 import {IsolatedContainer, PrimaryButton} from '../../shared/pattern';
 import {useQuery} from '../user/components/query';
 import styled from '@emotion/styled/macro';
+import {jsx} from '@emotion/core';
+
+const smallerHeading = {
+  marginTop: '10px',
+  marginBottom: '10px',
+};
+
+const heading = {
+  display: 'block',
+  fontFamily: 'inherit',
+  fontWeight: 500,
+  lineHeight: 1.1,
+};
 
 export const Text = styled.span(
   variantStyles({
@@ -9,6 +24,23 @@ export const Text = styled.span(
       faded: ({theme}) => ({color: theme.colors.faded}),
       fadedExtra: ({theme}) => ({color: theme.colors.fadedExtra}),
       background: ({theme}) => ({backgroundColor: theme.colors.backgroundFallback}),
+    },
+    size: {
+      subheading: [heading, smallerHeading, {fontSize: '25'}],
+    },
+  }),
+);
+
+export const Image = styled.img(
+  {border: 0, verticalAlign: 'middle'},
+  propStyles({
+    responsive: {
+      display: 'block',
+      maxWidth: '100%',
+      height: 'auto',
+    },
+    rounded: {
+      borderRadius: '6px',
     },
   }),
 );
@@ -28,14 +60,34 @@ function variantStyles(styles) {
 }
 
 function applyStyles(styles, props) {
-  console.log(styles);
-  return typeof styles === 'function' ? styles(props) : '';
+  return typeof styles === 'function'
+    ? styles(props)
+    : Array.isArray(styles)
+    ? styles.map((style) => applyStyles(style, props))
+    : styles;
 }
 
+function propStyles(styles) {
+  return function dynamicStyles(props) {
+    return Object.keys(props).map((key) => {
+      if (styles[key]) {
+        return applyStyles(styles[key], props);
+      }
+      return null;
+    });
+  };
+}
+
+//! keys are src, className, theme
+//! styles is an object {responsive: display, height, maxWidth }
+//!
 function Home({username}) {
   return (
     <IsolatedContainer>
-      <Text tint="background">Hello</Text>
+      <Text tint="background" size="subheading">
+        Hello
+        <Image src="https://github.com/tylermcginnis.png" responsive rounded />
+      </Text>
     </IsolatedContainer>
   );
 }
